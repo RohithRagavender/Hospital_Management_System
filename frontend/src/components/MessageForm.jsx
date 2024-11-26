5b3366de9bf5af72f9232f863029c0c891c65dc2
C.Rohith Ragavender <126795100+RohithRagavender@users.noreply.github.com>
2024-11-26 10:53:26 +0530

    Update MessageForm.jsx

--- frontend/src/components/MessageForm.jsx
+++ frontend/src/components/MessageForm.jsx
@@ -14,7 +14,7 @@ const MessageForm = () => {
     try {
       await axios
         .post(
-          "https://medify-f5p6.onrender.com",
+          "https://medify-f5p6.onrender.com/api/v1/message/send",
           { firstName, lastName, email, phone, message },
           {
             withCredentials: true,
