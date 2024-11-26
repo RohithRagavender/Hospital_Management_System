b2a553b317ea5c1b9052c0eccc683c38e38a9d48
C.Rohith Ragavender <126795100+RohithRagavender@users.noreply.github.com>
2024-11-26 10:55:17 +0530

    Update Register.jsx

--- frontend/src/Pages/Register.jsx
+++ frontend/src/Pages/Register.jsx
@@ -23,7 +23,7 @@ const Register = () => {
     try {
       await axios
         .post(
-          "https://medify-f5p6.onrender.com",
+          "https://medify-f5p6.onrender.com/api/v1/user/patient/register",
           { firstName, lastName, email, phone, nic, dob, gender, password },
           {
             withCredentials: true,
